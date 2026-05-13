ac8c4f37d929bac7db6ec8d9c921b33ec8e01627
coolsubbu <subbuyo@gmail.com>
2026-05-13 13:04:43 +0530

    Update and rename Clustering Algorithm.pl to clustering algorithm.pl

--- clustering algorithm.pl
+++ clustering algorithm.pl
@@ -1,7 +1,7 @@
 #!usr/bin/perl
 
 
-#AIM: To Cluster Images using correlation coefficients between the images.
+#aim: to cluster images using correlation coefficients between the images.
 $location="/home/yogesh/"
 opendir(DIR,"$location.corrnew") or die "the directory could not be found $!";   #directory containing the files containing the correlation coefficients between images
 open(FIL,">Clusters-version0.txt") or die "cant open the file $!";   #Output file to store the clustered image names
@@ -13,20 +13,20 @@ my %Head_hash;  #contains the member to head associations. used to avoid cases o
 my $maxcor=0;
 
 my %checkHash; #way to avoid duplicates for clustering elements.
-my %testHash;   #Contains all the unique elements
+my %testHash;   #contains all the unique elements
 my %Cluster; #contain all the clustered image names and its members.
 my %hash;   #the hash which stores the img_name;img_name=>correlation_coefficient
 
-foreach $file (@files)  #Read contents from all correlation files.
+foreach $file (@files)  #read contents from all correlation files.
 {
 
-    if($file =~/corrstage4-0/)    #we perform clustering for stage 4 for the Drosophila images.
+    if($file =~/corrstage4-0/)    #we perform clustering for stage 4 for the drosophila images.
     {
         open(FILE,"/home/yogesh/Desktop/corrnew/$file") or die "cant open the file $!";
         print "$file"."\npress...";
         #$m=<STDIN>;
         my @file_contents=<FILE>;
-        foreach $line (@file_contents)  #The format present in the file is: img_name1 gene1 stage Img_name2 gene2 corr_coef
+        foreach $line (@file_contents)  #The format present in the file is: img_name1 gene1 stage img_name2 gene2 corr_coef
         {
             my @words=split(/\t/,$line);
             #print @words;
@@ -49,29 +49,29 @@ foreach $file (@files)  #Read contents from all correlation files.
     }
 
     #$m=<STDIN>;
-   # print FIL "\nHASHED HEADERS";
+   # print FIL "\n hashed headers";
     foreach $img (keys %hash)
     {
- #       print FIL $img."\n";
+ #       print file $img."\n";
        # print $img."\n";       
     }
     
-    #Determining the threshold for Clustering. The correlation values above the threshold implies that the images belong to same group.
+    #determining the threshold for clustering. the correlation values above the threshold implies that the images belong to same group.
     my $avg=$sum/$total;
     
-  #  print  "\n\nTHE count is $count out of $total\nMAX value $maxcor \n max images are $maximg1 and $maximg2 \n average is $avg\n";
+  #  print  "\n\nthe count is $count out of $total\nMAX value $maxcor \n max images are $maximg1 and $maximg2 \n average is $avg\n";
 }   
    
    
    #####################
-   #CLUSTERING ALGORITHM.
+   # clustering algorithm #.
    #####################
    
-   # UNION_FIND structure $Head_hash is a member to Head Association structure. Every HEAD has its head value to be -1. Every member has its head value to be its Parent.
-   # Depending upon the strength of the correlation coefficients, We fill in this structure with the image names and cluster them by tracing the Head for every member.
-   # structure:  $Head_hash{$member}=$head;
+   # union_find structure $head_hash is a member to head association structure. every head has its head value to be -1. every member has its head value to be its parent.
+   # depending upon the strength of the correlation coefficients, we fill in this structure with the image names and cluster them by tracing the head for every member.
+   # structure:  $head_hash{$member}=$head;
    
-   # Initialize Union_Find Structure.
+   # initialize union_find structure.
    foreach $key (keys %testHash)
    {
     $Head_hash{$key}[0]="-0.1";
@@ -100,7 +100,7 @@ foreach $file (@files)  #Read contents from all correlation files.
                         $value=$hash{$key2}[0]{$key1}[0];
                     }
                     
-                    if($Head_hash{$key1}[0] eq "-0.1") #If key1 is new
+                    if($Head_hash{$key1}[0] eq "-0.1") #if key1 is new
                     {
 			print "both $key1 and $key2 are new\n";
 			
@@ -110,7 +110,7 @@ foreach $file (@files)  #Read contents from all correlation files.
                             $Head_hash{$key1}[1]=$value;
                             $Head_hash{$key2}[0]="-1";                        
                         }
-                        else    #key1 is new and key2 may be a Head or a Member => make key1 a member of key2
+                        else    #key1 is new and key2 may be a head or a member => make key1 a member of key2
                         {
                            $Head_hash{$key1}[0]=$key2;
                            $Head_hash{$key1}[1]=$value;
@@ -118,18 +118,18 @@ foreach $file (@files)  #Read contents from all correlation files.
                     }
                     else
                     {
-                        if($Head_hash{$key2}[0] eq "-0.1")   #key2 is new and key1 may be a Head or a Member => make key2 a member of key1
+                        if($Head_hash{$key2}[0] eq "-0.1")   #key2 is new and key1 may be a head or a member => make key2 a member of key1
                         {
                             $Head_hash{$key2}[0]=$key1;
                             $Head_hash{$key2}[1]=$value;
                             
                         }
-                        else   #BOTH ARE NOT NEW.. THEY COULD BE MEMBERS OR HEADS
+                        else   #both are not new ..... they could be either/both head or member
                         {
 			    
                             if($Head_hash{$key1}[0]!~/-1/)  #key1 is a member
                             {
-                                if($Head_hash{$key2}[0]!~/-1/)  #key2 is a member BOTH ARE MEMBERS
+                                if($Head_hash{$key2}[0]!~/-1/)  #key2 is a member both are members 
                                 {
 				    print "Both $key1 and $key2 are members \n";
                                     if($Head_hash{$key1}[0]>$value) #if key1's correlation coefficient to its Head is greater than that between key1 and key2 
@@ -148,7 +148,7 @@ foreach $file (@files)  #Read contents from all correlation files.
                                             $Head_hash{$key1}[0]=$key2;
                                             $Head_hash{$key1}[1]=$value;
                                         }
-                                        else  #if key1's and key2's correlation coefficient to their Head are lesser than that between key1 and key2 =>choose the one with larger coeff to its head and the other follows this one.
+                                        else  #if key1's and key2's correlation coefficient to their head are lesser than that between key1 and key2 =>choose the one with larger coeff to its head and the other follows this one.
                                         {
                                             if($Head_hash{$key1}[1]> $Head_hash{$key2}[1]) #key2 follows key1
                                             {
@@ -192,7 +192,7 @@ foreach $file (@files)  #Read contents from all correlation files.
                                         $Head_hash{$key2}[1]=$value;
                                     }
                                 }
-                                else  #BOTH ARE HEADS Choose a head to follow the other.
+                                else  #both are heads choose a head to follow the other.
                                 {
 				    print "Both $key1 and $key2 are heads \n";
                                     $Head_hash{$key1}[0]=$key2;
@@ -207,7 +207,7 @@ foreach $file (@files)  #Read contents from all correlation files.
         }
     }
    
- #Ithe correlation is below the threshold.
+ #the correlation is below the threshold.
  # if they are new make them as heads or dont bother
    foreach $key (keys %testHash)
     {
@@ -224,13 +224,13 @@ foreach $file (@files)  #Read contents from all correlation files.
    }
    print "press...\n";
    $my=<STDIN>;
-    #Enumerating the Clusters
-    # In order to enunciate the clusters, we need to find heads of all the nodes. This is done by FIND operations.
-    # We traverse the head for every member till the head becomes -1. The head becomes the member on the next iteration.
-    # The traversal is recursive and we need to prevent the recursion{Takes Too long!}
-    # we can create a reverse hash{maps Values to the Key}.
-    # Here the Reverse_Hash adds the members to their Heads{which are obtained from the Head_hash}
-    # It also adds the members of the members{if any(obtained from previous additions}} to the Head.
+    # enumerating the clusters
+    # in order to enunciate the clusters, we need to find heads of all the nodes. This is done by find operations.
+    # we traverse the head for every member till the head becomes -1. The head becomes the member on the next iteration.
+    # the traversal is recursive and we need to prevent the recursion{takes too long!}
+    # we can create a reverse hash{maps values to the key}.
+    # here the reverse_hash adds the members to their heads{which are obtained from the head_hash}
+    # it also adds the members of the members{if any(obtained from previous additions}} to the head.
    my %Reverse_hash;
    foreach $key (keys %testHash)
    {
